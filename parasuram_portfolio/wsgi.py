19d4fb5fc1029c73faa24e8df1f84b5d754ef8de
Parasuram2530 <parasuramgoud30@gmail.com>
2026-05-06 22:08:39 +0530

    Fix resume download fallback link

--- parasuram_portfolio/wsgi.py
+++ parasuram_portfolio/wsgi.py
@@ -1,6 +1,12 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+path = '/home/Parasuram/Portfolio'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parasuram_portfolio.settings')
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 app = application
